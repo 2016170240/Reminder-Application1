b986fd05c77eb611fb3a29026593c34315d33266
Abdulrahmen Emad <emadabdo258@gmail.com>
2020-03-28 00:50:34 +0200

    Updates

---
+++ precache-manifest.49bd4a4c27fd1004c4cb255b0e212a27.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fe76ff5190afd43e5dddfd639a36b531",
+    "url": "/vbg/index.html"
+  },
+  {
+    "revision": "927dffb03af6c56190b5",
+    "url": "/vbg/static/css/2.46b9f2a8.chunk.css"
+  },
+  {
+    "revision": "9fd7a86479fd127591e0",
+    "url": "/vbg/static/css/main.0c072401.chunk.css"
+  },
+  {
+    "revision": "927dffb03af6c56190b5",
+    "url": "/vbg/static/js/2.685d3e62.chunk.js"
+  },
+  {
+    "revision": "299cba5f6c747cbc295097594263041b",
+    "url": "/vbg/static/js/2.685d3e62.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9fd7a86479fd127591e0",
+    "url": "/vbg/static/js/main.e133bb97.chunk.js"
+  },
+  {
+    "revision": "f9faaba4d64f9153a9a8",
+    "url": "/vbg/static/js/runtime-main.60a03d2e.js"
+  }
+]);
